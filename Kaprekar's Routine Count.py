d3eb166119a56b4530d73a5cfb531150790cc62b
Scerva <100001899+Scerva@users.noreply.github.com>
2022-05-25 21:40:44 -0700

    Update Kaprekar's Routine Count.py

--- Kaprekar's Routine Count.py
+++ Kaprekar's Routine Count.py
@@ -3,7 +3,7 @@ class MoreThanFourDigits(Exception):
     pass
 class TooSimilar(Exception):
     """Raised when a four-digit number consists of the same digit."""
-
+    pass
 def kaprekar_count(n):
     """Finds the amount of 'Kaprekar's routines' it takes for a four-digit number to reach Kaprekar's constant: 6174."""
 
